0a1a412eaf6d2738188d20ee9dd10301e744b9af
Oreana <164940534+YazminAndazabal@users.noreply.github.com>
2024-11-24 13:53:45 -0300

    Update módulo-info.java

--- videojuegoo/src/application/módulo-info.java
+++ videojuegoo/src/application/módulo-info.java
@@ -1,1 +1,10 @@
-n
+module videojuegoo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+	requires java.desktop;
+	requires javafx.graphics;
+    
+    // Exporta el paquete application para que pueda ser usado por otros módulos, incluido javafx.graphics
+    opens application to javafx.fxml, javafx.graphics;
+}
